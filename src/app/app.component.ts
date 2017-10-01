3637f0561cbcafcdaaba18bbc78151d7e9b428f2
Tsuyoshi Yamaguchi <ronoya442@gmail.com>
2017-10-01 17:56:44 +0900

    AppComponentでタブの情報を表示

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  tab: chrome.tabs.Tab;
+
+  constructor(private ref: ChangeDetectorRef) { }
+
+  ngOnInit() {
+    // 現在のタブのオブジェクトを取得する
+    chrome.tabs.query({ 'active': true, 'lastFocusedWindow': true }, (tabs) => {
+      this.tab = tabs[0];
+
+      // 変更があったことを通知する
+      this.ref.detectChanges();
+    });
+  }
 }
